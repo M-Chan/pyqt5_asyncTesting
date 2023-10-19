de1f05081df640bb22907f0a45391665aa7306c0
Man Ting Chan <man.chan@diamond.ac.uk>
2023-10-19 13:39:54 +0100

    Start of the async
    This is a reproduce of a similar problem --> find the fix

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+import asyncio
 
 
-def window():
+async def window():
     app = QApplication(sys.argv)
     widget = QWidget()
 
     button1 = QPushButton(widget)
-    button1.clicked.connect(button1_clicked)
+    button1.clicked.connect(await button1_clicked)
 
     widget.show()
     sys.exit(app.exec_())
 
 
-def button1_clicked():
+async def button1_clicked():
     print("clicked")
 
 
 if __name__ == "__main__":
-    window()
+    asyncio.run(window())
